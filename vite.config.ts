ac9fd22f92b30451fda3f239b682d1931dd3094c
unknown <andreidanutmihai85@gmail.com>
2025-10-31 15:42:03 +0200

    s

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
         allowedHosts: true
-		allowedHosts: true
+		    
       },
       plugins: [react()],
       define: {
